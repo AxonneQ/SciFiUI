845d347015909e5d2776c734f410976b530e58d0
AxonneQ <40044656+AxonneQ@users.noreply.github.com>
2019-04-01 02:38:43 +0100

    Add abstract class UIElement for polymorphism

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -1,11 +1,15 @@
+/*
+        Student Name: Igor Bolek
+        Student Number: C17487376
+*/
+
 package ie.tudublin;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
 
 public class UI extends PApplet {
-        Button b;
-        MovingCircle mc;
-
+        // Input:
         boolean[] keys = new boolean[1024];
 
         public void keyPressed() {
@@ -13,42 +17,34 @@ public class UI extends PApplet {
         }
 
         public void keyReleased() {
-                keys[keyCode] = true;
+                keys[keyCode] = false;
         }
 
         public boolean checkKey(int c) {
                 return keys[c] || keys[Character.toUpperCase(c)];
         }
 
+        // Initial
+       ArrayList<UIElement> elements = new ArrayList<UIElement>();;
+
         public void settings() {
-                size(1200,1200,P3D);
-                // Use fullscreen instead of size to make your interface fullscreen
-                // fullScreen(P3D);
+                size(1200, 1200, P3D);
         }
-
+       
         public void setup() {
-                b = new Button(this, 50, 50, 100, 50, "I am a button");
-                mc = new MovingCircle(this, width / 2, height * .75f, 50);
-                radar = new Radar(this, 1, width / 2, height / 2, 100);
+                
+                UIElement r = new Radar(this, 1, width/2,height/2, 100);
+                UIElement b = new Button(this, 100,100,100,30,"Test string");
+
         }
-
-        Radar radar;
-
+        // Main loop
         public void draw() {
 
-        
-
-        /*        background(0);
-                b.render();
-
-                mc.update();
-                mc.render();
-
-                radar.update();
-                radar.render();
-
-                if (checkKey(LEFT)) {
-                        System.out.println("Left arrow key pressed");
-                }*/
+                background(0);
+                stroke(255);
+                for(UIElement e : elements){
+                        e.update();
+                        e.render();
+                }
         }
 }
